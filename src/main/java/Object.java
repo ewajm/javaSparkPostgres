2634552ff83ab6a91406496c9be4a7227b617679
Ewa Manek <manekej@gmail.com>
2016-09-25 09:48:38 -0700

    Add templates for db related methods

--- src/main/java/Object.java
+++ src/main/java/Object.java
@@ -1,3 +1,48 @@
 import java.util.ArrayList;
 import java.util.List;
 import org.sql2o.*;
+
+public class Object {
+  private int id;
+
+  public int getId(){
+    return id;
+  }
+
+  // @Override
+  // public boolean equals(Object otherObject){
+  //   if (!(otherObject instanceof Object)) {
+  //     return false;
+  //   } else {
+  //     Object newObject = (Object) otherObject;
+  //     return this.id == newObject.id;
+  //   }
+  // }
+
+  // public void save() {
+  //   try(Connection con = DB.sql2o.open()) {
+  //     String sql = "INSERT INTO objects () VALUES ()";
+  //     this.id = (int) con.createQuery(sql, true)
+  //       .addParameter("", this.)
+  //       .executeUpdate()
+  //       .getKey();
+  //   }
+  // }
+
+  // public static List<Object> all() {
+  //   String sql = "SELECT * FROM objects";
+  //   try(Connection con = DB.sql2o.open()) {
+  //     return con.createQuery(sql).executeAndFetch(Object.class);
+  //   }
+  // }
+
+  // public static Object find(int id) {
+  //   try(Connection con = DB.sql2o.open()) {
+  //     String sql = "SELECT * FROM objects where id=:id";
+  //     Object object = con.createQuery(sql)
+  //       .addParameter("id", id)
+  //       .executeAndFetchFirst(Object.class);
+  //     return object;
+  //   }
+  // }
+}
--- src/test/java/ObjectTest.java
+++ src/test/java/ObjectTest.java
@@ -5,12 +5,63 @@ import java.util.List;
 import org.sql2o.*;
 
 public class ObjectTest{
-  //REMEMBER TO RENAME!!!!!!1!one!
+  Object testObject;
   @Rule
   public DatabaseRule database = new DatabaseRule();
+
+  @Before
+  public void setUp(){
+    testObject = new Object();
+  }
 
   @Test
   public void methodBeingTested_behaviorBeingTested_ExpectedReturnType() {
     //assertEquals(expected-output, methodBeingTested(arguments));
   }
+
+  // @Test
+  // public void object_instantiatesCorrectly_true() {
+  //   assertTrue(testObject instanceof Object);
+  // }
+  //
+  // @Test
+  // public void equals_returnsTrueIfPropertiesAreSame_true(){
+  //   Object testObject2 = new Object();
+  //   assertTrue(testObject.equals(testObject2));
+  // }
+  //
+  // @Test
+  // public void save_insertsObjectIntoDatabase_Object() {
+  //   testObject.save();
+  //   Object testObject2 = null;
+  //   try(Connection con = DB.sql2o.open()){
+  //     testObject2 = con.createQuery("SELECT * FROM objects WHERE name=''")
+  //     .executeAndFetchFirst(Object.class);
+  //   }
+  //   assertTrue(testObject2.equals(testObject));
+  // }
+  //
+  // @Test
+  // public void all_returnsAllInstancesOfPerson_true() {
+  //   testObject.save();
+  //   Object testObject2 = new Object();
+  //   testObject2.save();
+  //   assertEquals(true, Object.all().get(0).equals(testObject));
+  //   assertEquals(true, Object.all().get(1).equals(testObject2));
+  // }
+  //
+  // @Test
+  // public void save_assignsIdToObject() {
+  //   testObject.save();
+  //   Object testObject2 = Object.all().get(0);
+  //   assertEquals(testObject.getId(), testObject2.getId());
+  // }
+  //
+  // @Test
+  // public void find_returnsObjectWithSameId_secondObject() {
+  //   testObject.save();
+  //   Object testObject2 = new Object();
+  //   testObject2.save();
+  //   assertEquals(Object.find(testObject2.getId()), testObject2);
+  // }
 }
